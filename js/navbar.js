d4c7a1c531d65641c7a0d428874bbca01587d456
Theng Rachana <vaingglorywar@gmail.com>
2023-08-31 09:44:32 +0700

    dynamic filter

--- js/navbar.js
+++ js/navbar.js
@@ -6,37 +6,21 @@ hamburger.onclick = function(){
 }
 
 
-// function send(){
-//     var getname = document.getElementById('mailname').value;
-//     var getmessage = document.getElementById('message').value;
-  
-//     localStorage.setItem('Name',getname);
-//     localStorage.setItem('Message',getmessage);
-//     alert("Thank you!");
-//   }
-
-
-  let search = "Opna Women's Short Sleeve Moisture";//
-  var button = document.getElementById('btn');
-  button.addEventListener("click", function(){
-    search = document.getElementById("search").value;
-    console.log(search);
+function filterProduct(value){
+  let buttons = document.querySelectorAll("#btn3");
+  buttons.forEach((button) => {
+  if(value.toUpperCase() == button.innerHTML.toUpperCase()){
+    button.classList.add("active");
+  }
+  else{
+    button.classList.remove("active");
+  }
   });
-  document.getElementById('products').append ( `<p>${search}</p>`);
   $(document).ready(() => {
     $('#products').html('');
     $.get("http://localhost:3000/Products", function(data, status){
       $.each(data, function(key, product) {
-            if(search == product.title){
-              // $("#products").append(`
-              // <img src="${product.image}">
-              // <h1>${product.title}</h1>`
-              // );
-            }
-            if(search != product.title){
-              
-            }
-            else{
+            if(value.toLowerCase() == product.category){
               $('#products').append(`
               <div class="items">
                 <div class="fiximg"><img src="${product.image}"></div>
@@ -46,8 +30,63 @@ hamburger.onclick = function(){
                 <button class="buy-btn">Buy Now</button>
                 </div>
             </div>
-          `);
-          }
+            `);
+            }
+            if(value.toLowerCase() == product.category){
+              $('#products').append(`
+              <div class="items">
+                <div class="fiximg"><img src="${product.image}"></div>
+                <h6>${product.title}</h6>
+                <div  class="btn-price">
+                <h5>$${product.price}</h5>
+                <button class="buy-btn">Buy Now</button>
+                </div>
+            </div>
+            `);
+            }
+            if(value.toLowerCase() == product.category){
+              $('#products').append(`
+              <div class="items">
+                <div class="fiximg"><img src="${product.image}"></div>
+                <h6>${product.title}</h6>
+                <div  class="btn-price">
+                <h5>$${product.price}</h5>
+                <button class="buy-btn">Buy Now</button>
+                </div>
+            </div>
+            `);
+            }
+            if(value.toLowerCase() == product.category){
+              $('#products').append(`
+              <div class="items">
+                <div class="fiximg"><img src="${product.image}"></div>
+                <h6>${product.title}</h6>
+                <div  class="btn-price">
+                <h5>$${product.price}</h5>
+                <button class="buy-btn">Buy Now</button>
+                </div>
+            </div>
+            `);
+            }
+            if(value == "all" || value == ""){
+              $('#products').append(`
+              <div class="items">
+                <div class="fiximg"><img src="${product.image}"></div>
+                <h6>${product.title}</h6>
+                <div  class="btn-price">
+                <h5>$${product.price}</h5>
+                <button class="buy-btn">Buy Now</button>
+                </div>
+            </div>
+            `);
+            }
+            
       });
     });
-});
+});    
+}
+
+
+window.onload = () => {
+  filterProduct("all");
+};
