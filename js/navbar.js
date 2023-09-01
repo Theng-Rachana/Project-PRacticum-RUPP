2608e3aae70b63d21efc2180b7ce4c011345cff9
Theng Rachana <vaingglorywar@gmail.com>
2023-09-02 00:01:28 +0700

    update registration form

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,9 @@
-//Navigation bar
-hamburger = document.querySelector(".hamburger");
-hamburger.onclick = function(){
-        navBar = document.querySelector(".nav-bar");
-        navBar.classList.toggle("active");
-}
+// //Navigation bar
+// hamburger = document.querySelector(".hamburger");
+// hamburger.onclick = function(){
+//         navBar = document.querySelector(".nav-bar");
+//         navBar.classList.toggle("active");
+// }
 
 
 function filterProduct(value){
@@ -59,3 +59,58 @@ window.onload = () => {
   filterProduct("all");
 };
 
+// Function to save user data in Local Storage
+function saveUser(username, password,email) {
+  const users = getUsers();
+  users.push({ username, password ,email});
+  localStorage.setItem('users', JSON.stringify(users));
+}
+
+// Function to retrieve user data from Local Storage
+function getUsers() {
+  return JSON.parse(localStorage.getItem('users')) || [];
+}
+
+// Function to check if a user with the same username exists
+function userExists(username) {
+  const users = getUsers();
+  return users.some(user => user.username === username);
+}
+
+// Login form
+const loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  
+  const username = document.getElementById('login-username').value;
+  const password = document.getElementById('login-password').value;
+  
+  const users = getUsers();
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (user) {
+      alert('Login successful!');
+  } else {
+      alert('Invalid username or password.');
+  }
+});
+
+// Register form
+const registerForm = document.getElementById('register-form');
+registerForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const username = document.getElementById('register-username').value;
+  const email = document.getElementById('register-email').value;
+  const password = document.getElementById('register-password').value;
+
+  if (userExists(username)) {
+      alert('Username already exists. Please choose a different one.');
+  } else {
+      saveUser(username, password,email);
+      alert('Registration successful!');
+      // Clear registration form fields
+      document.getElementById('register-username').value = '';
+      document.getElementById('register-email').value = '';
+      document.getElementById('register-password').value = '';
+  }
+});
